63bcd2d0d222030306888cad3a3d4fd3ba84f39f
kazmiek <kazmiek@gmail.com>
2023-10-25 15:55:51 +0200

    Flighter

--- src/main/java/com/danfoss/flighter/db/FlightStatus.java
+++
@@ -1,5 +0,0 @@
-package com.danfoss.flighter.db;
-
-public enum FlightStatus {
-    NEW, DELETED, IN_FLIGHT, ONBOARDING, GATE_CLOSED
-}
--- src/main/java/com/danfoss/flighter/db/FlightStatusView.java
+++
@@ -1,6 +0,0 @@
-package com.danfoss.flighter.db;
-
-public interface FlightStatusView {
-
-    FlightStatus getStatus();
-}
